8c2f270584950b115ee294fe59d4385b8bac2422
Abhi Dashadiya <abhi.d@simformsolutions.com>
2023-06-27 15:50:13 +0530

    add : test 1 and test 2

---
+++ Test1/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Test1.Controllers
+{
+	public class HomeController : Controller
+	{
+		public ActionResult Index()
+		{
+			ViewBag.Test = "Hello World!";
+			return View();   
+		}
+
+		public ActionResult Test2()
+		{
+			return View();
+		}
+
+
+	}
+}
